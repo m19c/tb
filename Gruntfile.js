246571f930c979ecd04dadf2e99773907e1053e8
MrBoolean <marcandrebinder@gmail.com>
2014-08-28 15:24:14 +0200

    Add wrong codeclimate coverage token

--- Gruntfile.js
+++ Gruntfile.js
@@ -18,7 +18,7 @@ module.exports = function (grunt) {
             coverage: {
                 options: {
                     file: 'dist/lcov.info',
-                    token: 'ec3ee6b4447b58b59a914d887acfef6706a0a072b36188b254fa54f48d24b9a5'
+                    token: ''
                 }
             }
         },
